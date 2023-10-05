bde6c51697e50b102641f0d31e4b646f039ee253
shivansh_chawla <chawlacodes@outlook.com>
2023-10-06 00:08:54 +0530

    organised files

--- Apache Beam/Txt - Txt Basic Pipeline/basic_pipeline.py
+++ Apache Beam/Txt - Txt Basic Pipeline/basic_pipeline.py
@@ -6,9 +6,9 @@ p1 = beam.Pipeline()
 
 data = {
 p1
-    | beam.io.ReadFromText(r"C:\Users\chawl\Projects\GCP_Projects\Apache Beam\records.txt", skip_header_lines=1)
+    | beam.io.ReadFromText(r"C:\Users\chawl\Projects\GCP_Projects\Apache Beam\Txt - Txt Basic Pipeline\records.txt", skip_header_lines=1)
     | beam.Map(lambda record: record.split(','))
-    | beam.Filter(lambda record: int(record[0])>4)
+    | beam.Filter(lambda record: int(record[0])>3)
     | beam.Filter(lambda record: str(record[1]=='Ended'))
     | beam.io.WriteToText(r"C:\Users\chawl\Projects\GCP_Projects\Apache Beam\output.txt")
 }
