424baba3ea803b14f3b05cffb3a439bdbf7f6874
shivansh_chawla <chawlacodes@outlook.com>
2023-10-19 05:12:20 +0530

    fixed basic_pipeline code

--- Apache Beam/Local-Beam-Local/Txt - Txt Basic Pipeline/basic_pipeline.py
+++ Apache Beam/Local-Beam-Local/Txt - Txt Basic Pipeline/basic_pipeline.py
@@ -6,10 +6,10 @@ p1 = beam.Pipeline()
 
 data = {
 p1
-    | "importing data" >> beam.io.ReadFromText(r"C:\Users\chawl\Projects\GCP_Projects\Apache Beam\Txt - Txt Basic Pipeline\records.txt", skip_header_lines=1)
+    | "importing data" >> beam.io.ReadFromText(r"C:\Users\chawl\Projects\GCP_Projects\Apache Beam\Local-Beam-Local\Txt - Txt Basic Pipeline\records.txt", skip_header_lines=1)
     | "splitting data" >> beam.Map(lambda record: record.split(','))
     | "duration filter" >> beam.Filter(lambda record: int(record[0])>3)
-    | "status filter" >> beam.Filter(lambda record: str(record[1]=='Ended'))
+    | "status filter" >> beam.Filter(lambda record: str(record[1])==" Ended")
     | "writing the data" >> beam.io.WriteToText(r"C:\Users\chawl\Projects\GCP_Projects\Apache Beam\output.txt")
 }
 
